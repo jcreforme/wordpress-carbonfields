a1a18ba720cf6aafe6d8ae0c45abb7a487722d8d
Juan Carlo <j.reforme@svitla.com>
2019-12-29 09:47:07 -0700

    controller updates

--- wp-content/themes/operam/app/Controllers/FrontPage.php
+++ wp-content/themes/operam/app/Controllers/FrontPage.php
@@ -6,5 +6,18 @@ use Sober\Controller\Controller;
 
 class FrontPage extends Controller
 {
+    public function getImages() {
 
+        // get the field value
+        $images = carbon_get_theme_option( 'images_list' );
+
+        foreach ( $images as $image ){
+            //print_r($image);
+            $value  = "<div class='image'>";   
+            $value .= wp_get_attachment_image( $image['image'], array('120', '120'), "", array( "class" => "img-responsive")); 
+            $value .= "</div>";
+        } 
+
+        return $value;
+    }
 }
--- wp-content/themes/operam/resources/views/partials/home-page.blade.php
+++ wp-content/themes/operam/resources/views/partials/home-page.blade.php
@@ -1,14 +1,7 @@
 <div class="homepage">
 <?php
-// get the field value
-/* $images = carbon_get_theme_option( 'images_list' );
-
-foreach ( $images as $image ){
-    //print_r($ator);
-    echo "<div class='image'>";   
-    echo wp_get_attachment_image( $image['image'], array('120', '120'), "", array( "class" => "img-responsive")); 
-    echo "</div>";
-} */
+    //get images from PHP controller
+    //echo $getImages;
 ?>
     <div id="app">  
         @{{template}}             
